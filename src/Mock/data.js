9198e17d9bbee31e256317244dcb92035b686ba8
stonyDeveloper <90153937+stonyDeveloper@users.noreply.github.com>
2023-02-01 16:09:16 +0100

    Update data.js

--- src/Mock/data.js
+++ src/Mock/data.js
@@ -1,31 +1,31 @@
-// const tasks = [
-//     {
-//      "createdAt": "2023-01-12T09:25:17.532Z",
-//      "name": "Ivan Gutkowski",
-//      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1079.jpg",
-//      "id": "1",
-//      "text": "Pick Up Laundry",
-//      "day": "Tomorrow",
-//      "reminder": ""
-//     },
-//     {
-//      "createdAt": "2023-01-12T09:31:51.315Z",
-//      "name": "Ralph Green",
-//      "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg",
-//      "id": "2",
-//      "text": "Study Redux",
-//      "day": "Tonight",
-//      "reminder": true
-//     },
-//     {
-//         "createdAt": "2023-01-12T09:31:51.315Z",
-//         "name": "Ralph Green",
-//         "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg",
-//         "id": "3",
-//         "text": "Sleep",
-//         "day": "Weekend",
-//         "reminder": true
-//        },
-// ]
+const tasks = [
+    {
+     "createdAt": "2023-01-12T09:25:17.532Z",
+     "name": "Ivan Gutkowski",
+     "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1079.jpg",
+     "id": "1",
+     "text": "Pick Up Laundry",
+     "day": "Tomorrow",
+     "reminder": ""
+    },
+    {
+     "createdAt": "2023-01-12T09:31:51.315Z",
+     "name": "Ralph Green",
+     "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg",
+     "id": "2",
+     "text": "Study Redux",
+     "day": "Tonight",
+     "reminder": true
+    },
+    {
+        "createdAt": "2023-01-12T09:31:51.315Z",
+        "name": "Ralph Green",
+        "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/513.jpg",
+        "id": "3",
+        "text": "Sleep",
+        "day": "Weekend",
+        "reminder": true
+       },
+]
 
-// export default tasks
+export default tasks
